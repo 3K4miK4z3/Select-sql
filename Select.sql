05cc4e68d8578827effcb039faad6bcd604684c0
Alisson Moreira <165946390+3K4miK4z3@users.noreply.github.com>
2024-10-02 16:58:03 -0300

    Add files via upload

---
+++ Select.sql
@@ -0,0 +1,41 @@
+-- Exercício 1 
+/*select nome_depto, sigla_depto from departamento*/
+    
+
+-- Exercício 2
+/*select distinct sigla_depto, nome_depto from departamento*/
+
+-- Exercício 3
+/*select nome_funcionario from funcionario*/
+
+-- Exercício 4
+/*select distinct nome_funcionario from funcionario*/
+
+-- Exercício 5
+/*select distinct nome_projeto, sigla_projeto from projeto*/
+
+-- Exercício 6
+/*select nome_depto from departamento where qtdfuncionariosdepto < 11;*/
+
+-- Exercício 7 
+/*select  nome_depto from departamento where qtdfuncionariosdepto >= 15;*/
+
+-- Exercício 8
+/*select nome_funcionario from funcionario where sigla_depto = 'IT';*/
+
+-- Exercício 9
+/*select nome_funcionario, cargo from funcionario order by cargo*/
+
+-- Exercício 10
+/*select nome_depto, sigla_depto from departamento order by sigla_depto*/ 
+
+-- Exercício 11
+/*select nome_projeto from projeto order by nome_projeto desc*/
+
+-- Exercício 12
+/*alter table funcionario
+add column data_emissao date,
+add column salario decimal(12,2);*/
+
+
+
