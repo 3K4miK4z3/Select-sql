4aaa286135675526114cb247b0bb671a9b88f248
Alisson Moreira <165946390+3K4miK4z3@users.noreply.github.com>
2024-10-04 10:35:29 -0300

    Add files via upload

---
+++ Select 1 e 2.sql
@@ -0,0 +1,19 @@
+-- SELECT 2
+
+-- Exercício 1
+/*select nome_depto from departamento where qtdfuncionariosdepto > 15*/
+
+-- Exercício 2
+/*select nome_funcionario from funcionario where salario >= 4.000*/
+
+-- Exercício 3
+/*select AVG (salario) from funcionario;*/
+
+-- Exercício 4
+/*select round (AVG(salario),2) from funcionario*/
+
+-- Exercício 5
+/*select distinct nome_funcionario from funcionario where nome_funcionario like '%gomes%'*/
+
+-- Exercício 6
+
